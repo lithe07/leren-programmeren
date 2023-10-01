2867ee755854e0c5ef3a8ada806e893a3cfb48cf
Lithe Jniad <99078068@mydavinci.nl>
2023-10-01 18:43:20 +0200

    commit

---
+++ module-2/Leerpaden/question.py
@@ -0,0 +1,8 @@
+naam = input("Wat is je naam? ")
+leeftijd = input(f"Goedendag {naam}, hoe oud ben je? ")
+werk = input(f"En als {leeftijd} jarige, wat doe je voor werk? ")
+info = input(f"En hoeveel dagen werk je als {werk}? ")
+
+SAMENVATITNG = f"De {leeftijd} jarige {naam} werkt als {werk} voor {info} dag/dagen per week. "
+
+print(SAMENVATITNG)
