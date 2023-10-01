d015594dbfdc09d6aba2f557654b8c0b5c71c80f
Lithe Jniad <99078068@mydavinci.nl>
2023-10-01 18:09:22 +0200

    commit

---
+++ module-2/Leerpaden/person.py
@@ -0,0 +1,13 @@
+
+naam = input("Voer uw naam in: ")
+adres = input("Voer uw adres in: ")
+postcode = input("Voer uw postcode in: ")
+woonplaats = input("Voer uw woonplaats in: ")
+
+
+print("----------------------------------------------------")
+print(f"|  Naam      : {naam}")
+print(f"|  Adres     : {adres}")
+print(f"|  Postcode  : {postcode}")
+print(f"|  Woonplaats: {woonplaats}")
+print("----------------------------------------------------")
