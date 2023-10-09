8ebffc1751cfa5832c0d09cd94c47641ec92179b
Lithe Jniad <99078068@mydavinci.nl>
2023-10-09 09:05:06 +0200

    commint

---
+++ module-3/Leerpaden/wie-is-de-gastheer.py
@@ -0,0 +1,12 @@
+MIJN_NAAM = "Lithe"
+SLB_NAAM = "Corbijn"
+
+gastheer = input("wie is de gastheer? ")
+gasten = 0
+drank = 0
+chips = 0
+
+if gasten or gastheer or (chips and drank) and gastheer == MIJN_NAAM and gastheer != SLB_NAAM == True:
+    print('Start the Party')
+else:
+    print('No Party')
