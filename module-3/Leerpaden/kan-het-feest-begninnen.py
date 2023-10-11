0bedf6cea4569baecc888715fcf08c1c068e7f94
Lithe Jniad <99078068@mydavinci.nl>
2023-10-11 09:07:17 +0200

    push

--- module-3/Leerpaden/kan-het-feest-begninnen.py
+++ module-3/Leerpaden/kan-het-feest-begninnen.py
@@ -1,11 +1,11 @@
-MIJN_NAAM = 'osama'
+MIJN_NAAM = 'lithe'
 SLB_NAAM = "jouke"
 
-gastheer = input('wie is de gastheer')
-gasten = int(input('hoeveel gasten'))
+gastheer = input('wie is de gastheer? ')
+gasten = int(input('hoeveel gasten? '))
 drank = 0
 chips = 0
-# print((gasten and chips and drank) or (gastheer > "" and drank))
+
 if (gastheer == MIJN_NAAM) or ((gasten and chips and drank) or (gastheer and drank)) and gastheer != SLB_NAAM or 20 >= gasten >= 4:
     print('Start the Party')
 else:
