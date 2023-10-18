0d0f4631fa7aee8e46a9730b16d8b22bc46548c8
Lithe Jniad <99078068@mydavinci.nl>
2023-10-18 22:33:12 +0200

    push

---
+++ module-1/Deel-4/word_bugs.py
@@ -0,0 +1,9 @@
+a = input('Geef een woord ')
+b = input('Geef nog een woord ')
+
+if a > b:     
+    print('Woord a heeft meer letters dan woord b')
+elif a < b:   
+    print('Woord a heeft minder letters dan woord b')
+else:                   
+    print('Woord a en woord b hebben evenveel letters')
