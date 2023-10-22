4f781f5d7c39f1d360e08008184b15d4c4b4e98b
Lithe Jniad <99078068@mydavinci.nl>
2023-10-22 16:06:57 +0200

    push

--- module-1/Deel-4/ice_bugs.py
+++ module-1/Deel-4/ice_bugs.py
@@ -1,11 +1,12 @@
-def convertToEuroText (amount):
+def convertToEuroText(amount):
     return "€{:.2f}".format(float(amount)).replace(".", ",")
 
-SMALL_PRICE = 1,25
-MEDIUM_PRICE = 2,10
+SMALL_PRICE = 1.25  # Gebruik een punt in plaats van een komma voor decimale getallen
+MEDIUM_PRICE = 2.10
 
-amount = input('Hoeveel ijsjes van {} wil je bestellen? '.format(convertToEuroText(SMALL_PRICE)))
-amount = input('En hoeveel ijsjes van {} wil je bestellen? '.format(convertToEuroText(MEDIUM_PRICE)))
-totalPrice = int(amount * SMALL_PRICE) + int(amount * MEDIUM_PRICE)
+amount_small = int(input('Hoeveel ijsjes van {} wil je bestellen? '.format(convertToEuroText(SMALL_PRICE))))
+amount_medium = int(input('En hoeveel ijsjes van {} wil je bestellen? '.format(convertToEuroText(MEDIUM_PRICE))))
+
+totalPrice = (amount_small * SMALL_PRICE) + (amount_medium * MEDIUM_PRICE)
 
 print('Dat is dan {} totaal'.format(convertToEuroText(totalPrice)))
