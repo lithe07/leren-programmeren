9d3bfda7a4e2341a2029937b4ea5bd48cc0af705
Lithe Jniad <99078068@mydavinci.nl>
2023-10-23 14:26:48 +0200

    push

--- module-1/Deel-4/pizza.py
+++ module-1/Deel-4/pizza.py
@@ -4,6 +4,7 @@
 
 def vraag_aantal(pizza_soort):
     while True:
+        print("uitgevoerd")
         try:
             aantal = float(input(f"Hoeveel {pizza_soort} pizza's wilt u bestellen? "))
             if aantal < 0:
@@ -21,19 +22,20 @@ PIZZA_PRIJZEN = {
 }
 
 # Hier is de invoer van de gebruiker voor pizza soorten
-aatnal_groot = float(input("Hoeveel groot pizza's wilt uw bestellen? "))
-aantal_medium = float(input("Hoeveel medium pizza's wilt uw bestellen? "))
-aantal_klein = float(input("Hoeveel klein pizza's wilt uw bestellen? "))
+aantal_groot = vraag_aantal("groot ") 
+aantal_medium = vraag_aantal("medium")
+aantal_klein = vraag_aantal("klein")
+
 
 # Berekeningen van pizza prijzen 
-kosten_groot = aatnal_groot * PIZZA_PRIJZEN["groot"]
+kosten_groot = aantal_groot * PIZZA_PRIJZEN["groot"]
 kosten_medium = aantal_medium * PIZZA_PRIJZEN["medium"]
 kosten_klein = aantal_klein * PIZZA_PRIJZEN["klein"]
 totaal_prijs = kosten_groot + kosten_medium + kosten_klein
 
 # dat is de bonnetje van de bestelde pizza van de gebruiker
 print("======= UW Bestelling=======")
-print(f"{aatnal_groot} x groot pizza's: € {kosten_groot}")
+print(f"{aantal_groot} x groot pizza's: € {kosten_groot}")
 print(f"{aantal_medium} x medium pizza': € {kosten_medium}")
 print(f"{aantal_klein} x klein pizza's: € {kosten_klein}")
 print("============================")
