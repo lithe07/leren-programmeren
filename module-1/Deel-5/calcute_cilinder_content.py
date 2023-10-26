dc3d5828222b12d6d8f6da266db33f3264cd063d
Lithe Jniad <99078068@mydavinci.nl>
2023-10-27 00:10:08 +0200

    opdracht-1

--- module-1/Deel-5/calcute_cilinder_content.py
+++ module-1/Deel-5/calcute_cilinder_content.py
@@ -1,16 +1,15 @@
-def calculate_cilinder_content(diameter: float,height: float) -> float:
-
-    from test_lib import test, report
-    from math_operations_start import test, report
+from math import pi 
+from test_lib import test, report
 
 
-
-
+def calculate_cilinder_content(diameter: float, height:float) -> float:
+    inhoud = pi * (diameter / 2) ** 2 * height
+    return inhoud
 
 diameter = 8.0
 height = 5.0
 expect_content = 251.3
-calculated_content = calculate_cilinder_content(diameter,height)
+calculated_content = round(calculate_cilinder_content(diameter,height),1)
 name = f'test diameter: {diameter} height: {height}'
 test(name, expect_content, calculated_content )
 
@@ -18,7 +17,7 @@ test(name, expect_content, calculated_content )
 diameter = 11.0
 height = 7.0
 expect_content = 665.2
-calculated_content = calculate_cilinder_content(diameter,height)
+calculated_content = round(calculate_cilinder_content(diameter,height),1)
 name = f'test diameter: {diameter} height: {height}'
 test(name, expect_content, calculated_content )
 
@@ -26,7 +25,7 @@ test(name, expect_content, calculated_content )
 diameter = 18.0
 height = 7.0
 expect_content = 1781.3
-calculated_content = calculate_cilinder_content(diameter,height)
+calculated_content = round(calculate_cilinder_content(diameter,height),1)
 name = f'test diameter: {diameter} height: {height}'
 test(name, expect_content, calculated_content )
 
@@ -34,7 +33,7 @@ test(name, expect_content, calculated_content )
 diameter = 15.0
 height = 2.0
 expect_content = 353.4
-calculated_content = calculate_cilinder_content(diameter,height)
+calculated_content = round(calculate_cilinder_content(diameter,height),1)
 name = f'test diameter: {diameter} height: {height}'
 test(name, expect_content, calculated_content )
 
@@ -42,7 +41,7 @@ test(name, expect_content, calculated_content )
 diameter = 0.0
 height = 6.0
 expect_content = 0.0
-calculated_content = calculate_cilinder_content(diameter,height)
+calculated_content = round(calculate_cilinder_content(diameter,height),1)
 name = f'test diameter: {diameter} height: {height}'
 test(name, expect_content, calculated_content )
 
