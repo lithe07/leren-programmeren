85bc7c5e0ca2380148143a5f261bbba66c1b1b8a
Lithe Jniad <99078068@mydavinci.nl>
2023-11-16 14:01:50 +0100

    push

--- module-2/Deel-1/De-tijd.py
+++ module-2/Deel-1/De-tijd.py
@@ -1,3 +1,16 @@
-for uur in range(1, 13):
-    for periode in ['AM', 'PM']:
-        print(f"{uur} {periode}")
+# for uur in range(1, 13):
+#     for periode in ['AM', 'PM']:
+#         print(f"{uur} {periode}")
+
+# for hour in range(24):
+#     suffix = "AM" if hour < 12 else "PM"
+#     display_hour = hour if hour <= 12 else hour - 12
+#     display_hour = 12 if display_hour == 0 else display_hour
+
+#     print(f"{display_hour:02d}:00 {suffix}")
+
+
+
+for hour in range(24):
+    uren_AM = 'AM' if uur < 12 else 'PM'
+    uren = uur if uur <= 12 else uur - 12
---
+++ module-2/Deel-1/forever-young/example6.py
@@ -0,0 +1,23 @@
+from RobotArm import RobotArm
+
+robotArm = RobotArm('exercise 6')
+
+
+robotArm.moveRight()
+
+for i in range(3):
+    for x in range(1):
+        robotArm.grab()
+        robotArm.moveLeft()
+        robotArm.drop()
+        robotArm.moveRight()
+
+    for c in range(1):
+        robotArm.grab()
+        robotArm.moveRight()
+        robotArm.drop()
+        robotArm.moveLeft()
+
+
+
+robotArm.wait()
