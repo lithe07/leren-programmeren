2860a9f7d849243f791fc3b5dfae6e1eecc2d057
Lithe Jniad <99078068@mydavinci.nl>
2023-11-21 22:58:57 +0100

    push

--- module-2/Deel-2/Dagen.py
+++ module-2/Deel-2/Dagen.py
@@ -1,35 +1,35 @@
 WEEK_DAGEN = ('maandag', 'dinsdag', 'woensdag', 'donderdag', 'vrijdag', 'zaterdag', 'zondag')
 
-# print('aantal dagen in de week: ')
+
 for dag in WEEK_DAGEN:
     print(dag, end=' ')
 print()
 
-# print("aantal werk dagen: ")
+
 for dag in WEEK_DAGEN:
     if dag.lower() not in ['zaterdag', 'zondag']:
         print(dag, end=' ')
 print()
 
 
-# print('weekenden: ')
+
 for dag in WEEK_DAGEN:
     if dag.lower() in ['zaterdag', 'zondag']:
         print(dag, end=' ')
 print()
 
-# print('aantal dagen in de week omgekeerd : ')
+
 for dag in reversed (WEEK_DAGEN):
     print(dag, end=' ')
 print()
 
-# print("aantal werk dagen omgekeerd: ")
+
 for dag in reversed(WEEK_DAGEN):
     if dag.lower() not in ['zaterdag', 'zondag']:
         print(dag, end=' ')
 print()
 
-# print('weekenden omgekeerd: ')
+
 for dag in reversed(WEEK_DAGEN):
     if dag.lower() in ['zaterdag', 'zondag']:
         print(dag, end=' ')
--- module-2/Deel-2/m&m's.py
+++ module-2/Deel-2/m&m's.py
@@ -7,7 +7,7 @@ aantal_mms_toevoegen = int(input("Hoeveel M&M's wil je aan de zak toevoegen? "))
 mms_zak = []
 
 for _ in range(aantal_mms_toevoegen):
-    willekeurige_kleur = random.choice(aantal_kleuren)
-    mms_zak.append(willekeurige_kleur)
+    kleur = random.choice(aantal_kleuren)
+    mms_zak.append(kleur)
 
 print(f"De gevulde M&M's zak: {mms_zak}")
