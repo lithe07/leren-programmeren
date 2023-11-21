929b8cafdb66d3f0fa1c6b7682fbba10e0404c35
Lithe Jniad <99078068@mydavinci.nl>
2023-11-21 23:16:36 +0100

    push

--- module-2/Deel-2/the-list-factory.py
+++ module-2/Deel-2/the-list-factory.py
@@ -1,16 +1,31 @@
+# aantal_lijstjes = int(input("Voer het aantal lijstjes in: "))
+
+# lijstjes = []
+
+# for i in range(1, aantal_lijstjes + 1):
+#     lengte_lijstje = int(input(f"Voer de lengte in voor lijst {i}: "))
+#     lijst = []  
+
+#     for j in range(lengte_lijstje):
+#         element = j + i  
+#         lijst.append(element)
+
+#     lijstjes.append(lijst)
+
+# for i, lijst in enumerate(lijstjes, start=1):
+#     print(f"Lijst {i}: {lijst}")
+
+
+
+
+
+# Vraag het aantal lijstjes
 aantal_lijstjes = int(input("Voer het aantal lijstjes in: "))
 
-lijstjes = []
-
+# Loop door het aantal lijstjes en vraag de lengte van elk lijstje
 for i in range(1, aantal_lijstjes + 1):
     lengte_lijstje = int(input(f"Voer de lengte in voor lijst {i}: "))
-    lijst = []  
 
-    for j in range(lengte_lijstje):
-        element = j + i  
-        lijst.append(element)
-
-    lijstjes.append(lijst)
-
-for i, lijst in enumerate(lijstjes, start=1):
-    print(f"Lijst {i}: {lijst}")
+    # Genereer en toon het lijstje met de gekozen lengte en stappen
+    lijstje = list(range(1, lengte_lijstje * i + 1, i))
+    print(f"Lijst {i}: {lijstje}")
