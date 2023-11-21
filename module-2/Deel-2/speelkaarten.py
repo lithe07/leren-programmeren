a40d777f19e51cf935cd9980137730deb493561a
Lithe Jniad <99078068@mydavinci.nl>
2023-11-21 22:48:11 +0100

    speelkarten 2

--- module-2/Deel-2/speelkaarten.py
+++ module-2/Deel-2/speelkaarten.py
@@ -2,14 +2,18 @@ import random
 
 kaarten = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Boer', 'Vrouw', 'Heer', 'Aas']
 soorten = ['Harten', 'Ruiten', 'Klaveren', 'Schoppen']
+jokers = ['joker', 'joker']
 
 deck = []
+
 for kaart in kaarten:
     for soort in soorten:
         huidige_kaart = f'{kaart} {soort}'
         deck.append(huidige_kaart)
 
+deck.extend(jokers)
 random.shuffle(deck)
+
 
 for i, kaart in enumerate(deck[:7]):
     print(f'Kaart {i+1}: {kaart}')
