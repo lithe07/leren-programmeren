f3720c205a2c8f88ba9bde9174565541a3e3e328
Lithe Jniad <99078068@mydavinci.nl>
2023-11-26 20:22:26 +0100

    Blastof

---
+++ medole-3/Blastoff.py
@@ -0,0 +1,10 @@
+import time
+
+teller = 30
+
+while teller > 0:
+    print(teller)
+    time.sleep(1)
+    teller -= 1
+
+print('Lancering!')
--- module-2/Deel-2/Dagen.py
+++ module-2/Deel-2/Dagen.py
@@ -1,36 +1,36 @@
 WEEK_DAGEN = ('maandag', 'dinsdag', 'woensdag', 'donderdag', 'vrijdag', 'zaterdag', 'zondag')
 
 
-for dag in WEEK_DAGEN:
-    print(dag, end=' ')
-print()
+# for dag in WEEK_DAGEN:
+#     print(dag, end=' ')
+# print()
 
 
-for dag in WEEK_DAGEN:
-    if dag.lower() not in ['zaterdag', 'zondag']:
-        print(dag, end=' ')
-print()
+for dag in WEEK_DAGEN[4::-1]:
+    # if dag.lower() not in ['zaterdag', 'zondag']:
+    #     print(dag, end=' ')
+    print(dag)
 
 
 
-for dag in WEEK_DAGEN:
-    if dag.lower() in ['zaterdag', 'zondag']:
-        print(dag, end=' ')
-print()
+# for dag in WEEK_DAGEN:
+#     if dag.lower() in ['zaterdag', 'zondag']:
+#         print(dag, end=' ')
+# print()
 
 
-for dag in reversed (WEEK_DAGEN):
-    print(dag, end=' ')
-print()
+# for dag in reversed (WEEK_DAGEN):
+#     print(dag, end=' ')
+# print()
 
 
-for dag in reversed(WEEK_DAGEN):
-    if dag.lower() not in ['zaterdag', 'zondag']:
-        print(dag, end=' ')
-print()
+# for dag in reversed(WEEK_DAGEN):
+#     if dag.lower() not in ['zaterdag', 'zondag']:
+#         print(dag, end=' ')
+# print()
 
 
-for dag in reversed(WEEK_DAGEN):
-    if dag.lower() in ['zaterdag', 'zondag']:
-        print(dag, end=' ')
-print()
+# for dag in reversed(WEEK_DAGEN):
+#     if dag.lower() in ['zaterdag', 'zondag']:
+#         print(dag, end=' ')
+# print()
