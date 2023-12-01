2ac2cd9a1165ab1fdf5d981e43331a99610a28f3
Lithe Jniad <99078068@mydavinci.nl>
2023-12-01 22:27:57 +0100

    example 12

--- module-2/Deel-1/forever-young/example11.py
+++ module-2/Deel-1/forever-young/example11.py
@@ -1,24 +1,22 @@
 from RobotArm import RobotArm
 robotArm = RobotArm('exercise 11')
 
-
-beweging = 9
-
 for a in range(8):
     robotArm.moveRight()
 
-for i in range(beweging - i):
+for i in range(9):
     robotArm.grab()
     color = robotArm.scan()
     if color == 'white':
         robotArm.moveRight()
         robotArm.drop()
-        robotArm.moveLeft()
-        robotArm.moveLeft()
+        if i < 8:
+            robotArm.moveLeft()
+            robotArm.moveLeft()
     else:
-        robotArm.drop(), robotArm.moveLeft()
-
-
+        robotArm.drop()
+        if i < 8:
+            robotArm.moveLeft()
 
 robotArm.wait()
  
