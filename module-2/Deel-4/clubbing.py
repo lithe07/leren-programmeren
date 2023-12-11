47431c0da1308f40f139151f73f365c0eb4544ce
Lithe Jniad <99078068@mydavinci.nl>
2023-12-11 11:18:49 +0100

    clubbing

--- module-2/Deel-4/clubbing.py
+++ module-2/Deel-4/clubbing.py
@@ -1,59 +1,51 @@
-
 PRIJS_COLA = 1.80
 PRIJS_BIER = 2.40
 PRIJS_CHAMPAGNE = 12.30
 
 DRANKJES = ('cola', 'bier', 'champagne')
 VIP_LIST = ('jeroen', 'jouke', 'rudi')
-prijs = 0
 
+BERICHT_1 = 'Alstublieft, complimenten van het huis'
+BERICHT_2 = 'Alsjebleif je drank dat is dan'
+BERICHT_3 = 'Sorry je mag geen alcohol bestellen onder de 21'
+BERICHT_4 = 'Probeer het over in jaar nog eens'
 
-#bouw hieronder de floowchart na
 print("Start programma")
 leeftijd = int(input("Hoe oud ben je? "))
 if leeftijd > 18:
-    naam = input("Wat is je naam? ")
+    naam = input('Wat is je naam? ')
     if naam in VIP_LIST:
         if leeftijd >= 21:
-            bandje = 'baluw'
-        else:
+            bandje = 'blauw'
+        else: 
             bandje = 'rood'
-        print(f"Je krijgt van mij een {bandje} bandje")
+        print(f'Je krijgt van mij een {bandje} bandje')
     else:
         if leeftijd >= 21:
             print("Je krijgt van mij een stempel")
-        else:
-            print("Je krijgt van mij geen stempel")     # Hier moeten we op terug 
-    drank = input("wat wil je drinken? ")
-    if drank in DRANKJES[0]:
-        if naam  in VIP_LIST:
-            drank = DRANKJES[0]
-        else:
-            prijs = PRIJS_COLA
+    drank = input("Wat wil je drinken? ")
+    if drank in DRANKJES:
+        if drank == 'cola':
+            if naam in VIP_LIST:
+                print(BERICHT_1) 
+            else:
+                print(BERICHT_2, PRIJS_COLA)
+        if drank == 'bier':
+            if naam in VIP_LIST and leeftijd >= 21:
+                print(BERICHT_1)
+            elif leeftijd >= 21:
+                    print(BERICHT_2, PRIJS_BIER)
+            else:
+                print(BERICHT_3)
+        if drank == 'champagne':
+            if naam in VIP_LIST and leeftijd >= 21:
+                print(BERICHT_2, PRIJS_CHAMPAGNE)
+            elif naam in VIP_LIST:
+                print(BERICHT_3)
+            else:
+                print("Sorry alleen vips mogen champagne bestellen")
     else:
-        if drank in DRANKJES[1] and leeftijd >= 21:
-            drank = DRANKJES[1]
-        else:
-            prijs = PRIJS_BIER
-            print(f"Alstublieft je {drank} dat is dan ${prijs}")
-        print('Alstublieft complimenten van het huis')
-          
-
-
-
-
-    # elif drank in DRANKJES[1]:
-    #     if naam in VIP_LIST or leeftijd >= 21:
-    #         drank = 'bier'
-    #     else:
-    #         prijs = PRIJS_BIER
-    #     print('Alstublieft complimenten van het huis')
-    # elif drank in DRANKJES[2]:
-    #     if naam in VIP_LIST and leeftijd >= 21:
-    #         drank = 'champagne'
-    #         prijs = PRIJS_CHAMPAGNE
-    #     else:
-    #         print('Sorry alleen vips mogen champagne bestellen')
-    #     print(f"Alstublieft je {drank} dat is dan ${prijs}")
-    # else:
-    #     print('Sorry geen idee wat je bedoeld, hier een glasje water')
+        print('Sorry geen idee wat je bedoeld, hier een glasje water')
+else:
+    print('Sorry je mag niet naar binnen')
+print("Einde programma")
