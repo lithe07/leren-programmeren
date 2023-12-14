466b856e809bc8a6d63664b5e8609124c39b3d43
Lithe Jniad <99078068@mydavinci.nl>
2023-12-14 14:36:33 +0100

    `boodschappenlijst

--- module-2/Deel-4/boodschappenlijst.py
+++ module-2/Deel-4/boodschappenlijst.py
@@ -1,18 +1,12 @@
 boodschappenlijst = {}
 
 while True:
-    item = input("Voer het item in (of 'stop' om te eindigen): ")
+    item = input("Voer het item in (of 'stop' om te eindigen): ").lower()
 
     if item == 'stop':
         break
 
     hoeveelheid = int(input("Voer de hoeveelheid in: "))
-
-    # Controleer of het item al in de lijst staat, ongeacht de hoofdletters
-    for bestaand_item in boodschappenlijst:
-        if bestaand_item == item:
-            item = bestaand_item
-            break
 
     if item in boodschappenlijst:
         boodschappenlijst[item] += hoeveelheid
