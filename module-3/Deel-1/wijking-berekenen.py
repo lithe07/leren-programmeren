10c5ab3debd22cc0a1ad3d107036dcb334129827
Lithe Jniad <99078068@mydavinci.nl>
2024-02-08 18:48:15 +0100

    Standaardafwijking berekenen

---
+++ module-3/Deel-1/wijking-berekenen.py
@@ -0,0 +1,13 @@
+import math
+
+def Standaardafwijking_berekenen(getallen):
+    aantal = len(getallen)
+    gemiddelde = sum(getallen) / aantal
+    verschil_kwadraat = sum((x - gemiddelde) ** 2 for x in getallen)
+    variantie = verschil_kwadraat / aantal
+    standaardafwijking = math.sqrt(variantie)
+    return standaardafwijking, variantie
+
+getallenlijst = [16, 2, 5, 8, 12, 3, 9, 16, 5, 8, 64, 33]
+berekening, variantie =  Standaardafwijking_berekenen(getallenlijst)
+print("De standaard afwijkingen van de getallen zijn:", berekening)
