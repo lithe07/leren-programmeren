65a1562d7f02859ec21181f4f6fd74ca44d80141
Lithe Jniad <99078068@mydavinci.nl>
2024-02-08 19:54:29 +0100

    woonplaats

---
+++ module-3/Deel-1/woonplaats.py
@@ -0,0 +1,23 @@
+def naam_en_leeftijd():
+    naam = input("Wat is je naam? ")
+    leeftijd = int(input("Wat is je leeftijd? "))
+    woonplaats = input("Waar woon je? ")
+    gegevens = {
+        'naam': naam,
+        'leeftijd' : leeftijd,
+        'woonplaats': woonplaats
+    } 
+    return gegevens
+
+def gegevens_verzamelen():
+    verzamel = []
+    while True:
+        stop_of_door = input("Wil je stoppen (j/n)? ")
+        if stop_of_door == "j":
+            break
+        verzamel.append(naam_en_leeftijd())
+    return  verzamel
+
+data = gegevens_verzamelen()
+for i in range(len(data)):
+    print(f"{data[i]['naam']}, die in {data[i]['woonplaats']} woont, is {data[i]['leeftijd']} jaar")
