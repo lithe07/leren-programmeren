4f30e35ca9a8400e5897153d1e143c96cf521e63
Lithe Jniad <99078068@mydavinci.nl>
2024-02-08 19:18:07 +0100

    fibonacci

--- module-3/fibonacci.py
+++ module-3/fibonacci.py
@@ -1,31 +1,11 @@
-def fibonacci(n):
-    """ Bereken de Fibonacci-reeks tot het n-de getal en retourneer de reeks."""
-    fib_sequence = [0, 1]  # Initialisatie van de Fibonacci-reeks met de eerste twee getallen
+def fibonacci(getal): 
+    getallen_lijst = [0, 1]
+    for i in range(2, aantal):
+        volgende_getal = getallen_lijst[-1] + getallen_lijst[-2]
+        getallen_lijst.append(volgende_getal)
+    return getallen_lijst
 
-    while len(fib_sequence) < n:
-        fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])  # Voeg het volgende Fibonacci-getal toe aan de reeks
+aantal = 10
+fibonacci_uitkomst = fibonacci(aantal)
+print(fibonacci_uitkomst)
 
-    return fib_sequence
-
-def golden_ratio(fib_sequence):
-    """ Bereken de gulden snede op basis van de laatste twee getallen uit de Fibonacci-reeks."""
-    if len(fib_sequence) >= 2:
-        last_two_numbers = fib_sequence[-2:]  # Selecteer de laatste twee getallen uit de Fibonacci-reeks
-        golden_ratio = last_two_numbers[-1] / last_two_numbers[-2]  # Bereken de gulden snede
-        return golden_ratio
-    else:
-        return "Fibonacci-reeks heeft niet genoeg getallen voor het berekenen van de gulden snede."
-
-# Vraag de gebruiker om het aantal getallen in de Fibonacci-reeks in te voeren
-n = int(input("Voer het aantal getallen in de Fibonacci-reeks in: "))
-
-# Bereken de Fibonacci-reeks
-fib_seq = fibonacci(n)
-
-# Toon de Fibonacci-reeks
-print("De Fibonacci-reeks:")
-print(fib_seq)
-
-# Bereken en toon de gulden snede
-golden_ratio_value = golden_ratio(fib_seq)
-print("De gulden snede op basis van de laatste twee getallen uit de Fibonacci-reeks is:", golden_ratio_value)
